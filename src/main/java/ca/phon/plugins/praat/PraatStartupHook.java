65e364b94b7c0ee0bc2b2777821678d32d342d70
Gregory Hedlund <greg.hedlund@gmail.com>
2014-06-27 06:49:04 -0230

    Initialize praat library on phon startup

---
+++ src/main/java/ca/phon/plugins/praat/PraatStartupHook.java
@@ -0,0 +1,45 @@
+package ca.phon.plugins.praat;
+
+import java.util.logging.Logger;
+
+import ca.hedlund.jpraat.binding.Praat;
+import ca.phon.app.hooks.PhonStartupHook;
+import ca.phon.plugin.IPluginExtensionFactory;
+import ca.phon.plugin.IPluginExtensionPoint;
+import ca.phon.plugin.PluginException;
+
+/**
+ * Init Praat library on startup.
+ *
+ */
+public class PraatStartupHook implements PhonStartupHook, IPluginExtensionPoint<PhonStartupHook> {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(PraatStartupHook.class.getName());
+	
+	@Override
+	public void startup() throws PluginException {
+		LOGGER.info("Initializing Praat library");
+		Praat.INSTANCE.praat_lib_init();
+	}
+
+	@Override
+	public Class<?> getExtensionType() {
+		return PhonStartupHook.class;
+	}
+
+	@Override
+	public IPluginExtensionFactory<PhonStartupHook> getFactory() {
+		return factory;
+	}
+	
+	private final IPluginExtensionFactory<PhonStartupHook> factory = new IPluginExtensionFactory<PhonStartupHook>() {
+		
+		@Override
+		public PhonStartupHook createObject(Object... args) {
+			return PraatStartupHook.this;
+		}
+		
+	};
+
+}
