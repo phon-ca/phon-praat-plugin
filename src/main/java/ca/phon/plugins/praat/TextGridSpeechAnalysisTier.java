7861549b8a4a737540046a3957952c0fd88eda7d
Greg Hedlund <greg.hedlund@gmail.com>
2020-04-28 09:32:39 -0230

    Request application focus after TextGrid is sent back to phon

--- src/main/java/ca/phon/plugins/praat/TextGridSpeechAnalysisTier.java
+++ src/main/java/ca/phon/plugins/praat/TextGridSpeechAnalysisTier.java
@@ -1150,6 +1150,10 @@ public class TextGridSpeechAnalysisTier extends SpeechAnalysisTier {
 				}
 				unlock();
 				update();
+				
+				if(Desktop.isDesktopSupported())
+					Desktop.getDesktop().requestForeground(true);
+				getParentView().getEditor().requestFocus();
 			}
 		}
 		
